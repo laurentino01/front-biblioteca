fcd1713aafcc4700f52dda331b84da4a81ced5ce
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-08-10 22:18:40 -0300

    connect with api

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import Header from "../../components/Header/Header";
 import "./index.scss";
 
 const Home = () => {
   return (
-    <div className='home'>      
-      <Header/>
+    <div className="home">
+      <Header />
       <h1>Biblioteca Central Online - Livros</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
